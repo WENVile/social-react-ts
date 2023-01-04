5a19dea93d731212e5aea57568f8bf31dca80184
Oleksandr Ohurtsov <oleksandr.ohurtsov@gmail.com>
2023-01-04 18:47:30 +0200

    after 1-7 lessons add 2 new components Header, Tecnologies

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies />
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
+        <div className="App">
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div>
+            <a href='#'>Home</a>
+            <a href='#'>New Feed</a>
+            <a href='#'>Messages</a>
+        </div>
+    )
 }
 
 export default App;
